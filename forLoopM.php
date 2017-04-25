9b8b73d4714520a59f57b479b6b06f85a9a44147
Caprica0606 <caprica0606@gmail.com>
2017-04-25 14:35:26 -0400

    made some text corrections

---
+++ forLoopM.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="codingCommanders.css">
+<title>Coding Commanders</title>
+<style>
+p{
+  margin-left: 100px;
+  margin-right:100px;
+  color: black;
+  font-style: Helvetica;
+  font-size: 20px;
+  line-height: 200%;
+  text-emphasis: bolder;
+}
+h3{
+  color: #443700;
+}
+</style>
+</head>
+<body background = "transLogo.png">
+<!-- top menu-->
+  <div class = "menuDiv">
+    <span class = "row">
+      <a class = "nav" href = "codingCommanders.php">Home</a> <a class ="nav" href ="curriculum.php">Curriculum</a>  <a class = "nav" href = "https://www.youtube.com/channel/UC-gCUMK_EGUqJ7P9VA7BJmQ">YouTube Channel</a> <a class = "nav" href = "blog.html">B l o g</a>
+    </span>
+   </div>
+   <div col-6 align = "center">
+     <h1 font-famil = "Lucida Console">for loops</h1>
+     <img src = "forLoop.PNG"><img>
+     <h5 font-family = "Lucida Console">for loop syntax</h5>
+     <h3>"Init counter"</h3><p> This is where you intialize your counter.
+             The number you set the init counter to will be where your loop starts counting.</p>
+     <h3>"Test Counter"</h3><p> This tests for a condition.  If the condition is met, your loop will continue.</p>
+     <h3>"Increment Counter"</h3><p> When the loop is executed, this is what tells the loop how to modify the counter.
+       For example you may want to increment, decrement or perform another mathematical operation.</p>
+     <br>
+     <img align = "center" src = "forLoopSyn.PNG"><img>
+     <h5 font-family = "Lucida Console">for loop example</h5>
+     <img src = "forLoopExample.PNG"><img>
+     <h5 font-family = "Lucida Console">for loop to print an array</h5>
+     <img src = "forLoopPrint.PNG"><img>
+   </div>
+   <br><br>
+   <!-- Code Editior-->
+  <iframe frameborder="0" width="100%" height="500px" src="https://repl.it/HZzv/1"></iframe>
+</body>
+</html>
